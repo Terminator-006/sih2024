3f0e59a3b50e959c658563db47418df3df346cbb
Siya Mittal <del.siyamittal@gmail.com>
2024-09-06 10:36:56 +0530

    made spinner bg black

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Spinner = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-100 z-50">
       <div className="flex flex-col items-center">
         <div className="w-16 h-16 rounded-full animate-spin-gradient bg-gradient-to-r from-red-400 via-yellow-400 to-blue-400"></div>
         <p className='text-white mt-4'>Life begins at the end of your comfort zone</p>
